106f94ffc2270c4d73bd25bdce0b81ac9be96c67
asflash8 <asflash8@gmail.com>
2017-12-05 15:06:15 +0900

    bin/hanami g action web authentication#new --url=/authentication/new

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -5,3 +5,4 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/', to: 'explorer#index'
 post '/', to: 'explorer#execute'
+get '/authentication/new', to: 'authentication#new'
