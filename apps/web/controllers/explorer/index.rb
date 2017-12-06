8065d4f1a94e2ade1220008ce02b7b6b02df394c
asflash8 <asflash8@gmail.com>
2017-12-06 19:52:32 +0900

    認証判定処理を追加

--- apps/web/controllers/explorer/index.rb
+++ apps/web/controllers/explorer/index.rb
@@ -2,11 +2,21 @@ module Web::Controllers::Explorer
   class Index
     include Web::Action
 
+    before :authenticate!
     expose :endpoints
+
     def call(params)
       @endpoints = Endpoint.all.each_with_object({}){|(target, methods), obj|
         methods.map{|method, name| obj[name] = target + "_" + method}
       }
+    end
+
+    private
+    def authenticate!
+      result = UserInteractor::Authenticator.new(session).call
+      if result.failure?
+        redirect_to '/authentication/new'
+      end
     end
   end
 end
